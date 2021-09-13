5aad6d6a52851a49ca48abe3a08ad7045dedce6e
Great-Stone <hahohh@gmail.com>
2021-09-13 12:39:20 +0900

    add 01_network

---
+++ 01_network/main.tf
@@ -0,0 +1,40 @@
+locals {
+  tags = {
+    Owner        = "gslee"
+    Region       = var.region
+    Purpose      = "Snapshot"
+    TTL          = 72
+    Terraform    = true
+    TFE          = true
+    TFEWorkspace = "snapshot-auth/network"
+  }
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = ">= 3.58.0"
+    }
+  }
+}
+
+provider "aws" {
+  region                  = var.region
+  access_key              = var.access_key
+  secret_key              = var.secret_key
+
+  default_tags = local.tags
+}
+
+resource "aws_security_group" "example" {
+  name        = "example"
+  description = "Allow SSH port from all"
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
