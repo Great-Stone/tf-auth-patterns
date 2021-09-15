b6855cfdba0a863480e5a500f0c9c2c402ce0044
Great-Stone <hahohh@gmail.com>
2021-09-15 10:50:13 +0900

    edit variable from sts

--- 03_vault_aws_auth/terraform.tf
+++ 03_vault_aws_auth/terraform.tf
@@ -9,7 +9,7 @@ resource "tfe_workspace" "ws" {
 
 resource "tfe_variable" "access_key" {
   key          = "AWS_ACCESS_KEY_ID"
-  value        = data.vault_generic_secret.cred.data.access_key
+  value        = data.vault_aws_access_credentials.tfc.access_key
   category     = "env"
   workspace_id = tfe_workspace.ws.id
   description  = "aws access key from vault"
@@ -17,7 +17,16 @@ resource "tfe_variable" "access_key" {
 
 resource "tfe_variable" "secret_key" {
   key          = "AWS_SECRET_ACCESS_KEY"
-  value        = data.vault_generic_secret.cred.data.secret_key
+  value        = data.vault_aws_access_credentials.tfc.secret_key
+  category     = "env"
+  workspace_id = tfe_workspace.ws.id
+  sensitive    = true
+  description  = "aws access key from vault"
+}
+
+resource "tfe_variable" "security_token" {
+  key          = "AWS_SESSION_TOKEN"
+  value        = data.vault_aws_access_credentials.tfc.security_token
   category     = "env"
   workspace_id = tfe_workspace.ws.id
   sensitive    = true
--- 03_vault_aws_auth/vault.tf
+++ 03_vault_aws_auth/vault.tf
@@ -1,7 +1,17 @@
-data "vault_generic_secret" "cred" {
-  path = "aws/creds/my-role"
+// data "vault_generic_secret" "cred" {
+//   path = "aws/creds/my-role"
+// }
+
+data "vault_aws_access_credentials" "tfc" {
+  backend = "aws"
+  type    = "sts"
+  role    = "ec2_admin"
 }
 
-output "vault_aws_cred" {
-  value = nonsensitive(data.vault_generic_secret.cred.data)
+// output "vault_aws_cred" {
+//   value = nonsensitive(data.vault_generic_secret.cred.data)
+// }
+
+output "vault_aws_sts" {
+  value = data.vault_aws_access_credentials.tfc
 }
